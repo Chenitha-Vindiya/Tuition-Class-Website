71848c9001d912be84f2c94d9c378068f609af95
Chenitha Vindiya <chenithavindiya@gmail.com>
2025-08-17 03:32:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
+    
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -392,5 +394,6 @@
     <script src="/My/JS/search-bar.js"></script>
 
 </body>
+
 
 </html>
